3584d0a15e679c16abf2d596e5925003c1deedaf
uysha-doge <uyshandr@gmail.com>
2026-02-13 22:19:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,35 @@ from fastapi.middleware.cors import CORSMiddleware
 import asyncio
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
+from sqlalchemy.orm import DeclarativeBase, relationship
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy import BigInteger, String, ForeignKey
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
+
+DB_URL = 'sqlite:///db/pierre.db'
+engine = create_engine(DB_URL, echo=True)
+
+Session = sessionmaker(engine)
+
+class Base(DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ = 'users'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    tg_id = mapped_column(BigInteger)
+    
+
+class Books(Base):
+    __tablename__ = 'books'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title = mapped_column(String(35))
+    ttl_pages = mapped_column(String(4))
+    cur_pages = mapped_column(String(4))
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
 
 async def lifespan(app_: FastAPI):
     print("Bot is ready")
@@ -19,4 +47,3 @@ async def glavstr():
         html_content = f.read()
 
     return HTMLResponse(content=html_content)
-
